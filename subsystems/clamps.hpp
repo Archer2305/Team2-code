f4e7bc5affda2a421860867a232d8c0b41669767
Archer2305 <73808514+Archer2305@users.noreply.github.com>
2022-05-01 12:16:24 -0700

    Add files via upload
    added header subsystems

---
+++ subsystems/clamps.hpp
@@ -0,0 +1,8 @@
+#include "main.h"
+
+extern pros::ADIDigitalOut fourBarClamp;
+extern pros::ADIDigitalIn fourBarSwitch;
+extern pros::ADIDigitalIn fourBarSwitch2;
+extern pros::ADIDigitalIn fourBarBumper;
+
+void updateFourBarClamp();
---
+++ subsystems/ports.hpp
@@ -0,0 +1,17 @@
+#include "main.h"
+//drivetrain ports
+const int rightFrontPort = 21;
+const int rightTopPort = 4;
+const int rightBottomPort = 3;
+const int leftFrontPort = 18;
+const int leftTopPort = 10;
+const int leftBottomPort = 2;
+const int fourBarPort = 1;
+const int conveyorPort = 13;
+const int inertialPort = 14;
+const int inertial2Port = 17;
+const char fourBarSwitchPort = 'G';
+const char fourBarSwitchPort2 = 'H';
+const char fourBarClampPort = 'A';
+const char tilterClampPort = 'C';
+const char tilterPort = 'B';
